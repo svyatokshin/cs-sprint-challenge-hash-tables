fbda62946c36d1932165673876d3c137e9d74e7b
svyatoslav-okshin <svyatokshin@gmail.com>
2020-09-12 13:42:41 -0400

    finished example 4 and passed tests

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -1,10 +1,24 @@
-def has_negatives(a):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
+# Given a list of integers both positive and negative
+# Want to figure out what positive numbers have corresponding negative numbers in the same list
 
-    return result
+def has_negatives(a):
+    cache = {}
+    list1 = []
+    # convert all numbers in list to positive ones using abs()
+    a = [abs(i) for i in a]
+
+    for i in a:
+        if i not in cache:
+            cache[i] = 1  # if not in cache we set equal to 1
+
+        else:
+            cache[i] += 1  # if in cache already we add 1
+
+    for key in cache:
+        if cache[key] == 2:  # if theres 2 for a specific key, we append to list and return
+            list1.append(key)
+
+    return list1
 
 
 if __name__ == "__main__":
